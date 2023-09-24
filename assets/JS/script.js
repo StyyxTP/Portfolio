36a9f1fe8d01872959357de3d6844cc033049ba6
Styyx <maxime.mimault.pro@gmail.com>
2023-09-24 18:03:12 +0200

    ajout de mon portfolio sur git

---
+++ assets/JS/script.js
@@ -0,0 +1,21 @@
+// dark/light mode
+const themeToggleBtn = document.querySelector('.header__light_dark_mode_toggle');
+themeToggleBtn.addEventListener('click', () => {
+    document.body.classList.toggle('light-mode');
+});
+
+
+// permet de faire apparaitre mes items choisis
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
